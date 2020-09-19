55efc05b869b1f16c252126e379d6054bed4836a
Tuan Le <tuanldt@outlook.com>
2020-09-19 23:01:33 +0200

    populate UsersPage.tsx, get rid of unnecessary styles

--- pages/UsersPage.tsx
+++ pages/UsersPage.tsx
@@ -1,14 +1,24 @@
+// dependencies
+import Link from "next/link";
 import useSWR from "swr";
 import { gql } from "graphql-request";
 
+// atlaskit
+import DynamicTable from "@atlaskit/dynamic-table";
+
+// styles
+
+// components
 import Layout from "../layout/Layout";
 
-import { fetcher } from '../modules/api';
+// data fetching
+import { fetcher } from "../modules/api";
 
 export default function UsersPage() {
   const { data, error } = useSWR(
-    gql`{
-      UserList {
+    gql`
+      {
+        UserList {
           results {
             id
             email
@@ -16,30 +26,77 @@ export default function UsersPage() {
             firstName
             lastName
           }
+        }
       }
-    }`,
+    `,
     fetcher
   );
 
   // TODO: Loading.
-  const users = data?.UserList?.results || []
+  const users = data?.UserList?.results || [];
 
+  // Generating user table header
+  const tableHeaderNames = ["Name", "Username", "Email"];
+
+  const mappedTableHead = tableHeaderNames.map((headerNames, i) => ({
+    key: headerNames,
+    isSortable: true,
+    shouldTurncate: false,
+    content: headerNames,
+    // testId: headerNames,
+  }));
+
+  const tableHeadRow = {
+    cells: mappedTableHead,
+  };
+
+  // Generating user table rows
+  const tableRows = users.map((user, i) => ({
+    cells: [
+      {
+        // Name
+        key: user.lastName + user.firstName,
+        content: (
+          <Link href={`/users/${encodeURIComponent(user.id)}`}>
+            <a>
+              {user.lastName} {user.firstName}
+            </a>
+          </Link>
+        ),
+      },
+      {
+        // Username
+        key: user.username,
+        content: user.username,
+      },
+      {
+        // Email
+        key: user.email,
+        content: user.email,
+      },
+    ],
+    key: user.username,
+  }));
+
+  // TODO: Come up with a prettier design, this is really ugly.
   return (
     <Layout>
       <h1>Users</h1>
       <section>
-        {
-          users
-            .map((user) => {
-              console.log(user)
-              return (
-                <div key={user.id} className='users-container'>
-                  <a className='user' href={`/users/${user.id}`}>{user.firstName} {user.lastName} ({user.username})</a>
-                  <br />
-                </div>
-              )
-            })
-        }
+        <DynamicTable
+          caption={null}
+          head={tableHeadRow}
+          rows={tableRows}
+          // rowsPerPage={10}
+          // defaultPage={1}
+          loadingSpinnerSize="large"
+          isLoading={false}
+          isFixedSize
+          defaultSortKey="Name"
+          defaultSortOrder="ASC"
+          onSort={() => console.log("onSort")}
+          onSetPage={() => console.log("onSetPage")}
+        />
       </section>
     </Layout>
   );
