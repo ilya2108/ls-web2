7abe23c59eb94b7916e81b70097cc096bbd1cdbe
Tuan Le <tuanldt@outlook.com>
2020-09-24 18:47:13 +0200

    add spinner to UsersPage

--- pages/UsersPage.tsx
+++ pages/UsersPage.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 import useSWR from "swr";
 import { gql } from "graphql-request";
 import DynamicTable from "@atlaskit/dynamic-table";
-import PageHeader from '@atlaskit/page-header';
+import PageHeader from "@atlaskit/page-header";
 import Layout from "../layout/Layout";
-import { BreadcrumbsItem, BreadcrumbsStateless } from '@atlaskit/breadcrumbs';
+import { BreadcrumbsItem, BreadcrumbsStateless } from "@atlaskit/breadcrumbs";
 import { fetcher } from "../modules/api";
+import HugeSpinner from "../components/HugeSpinner/HugeSpinner";
 
 export default function UsersPage() {
   const { data, error } = useSWR(
@@ -43,63 +44,72 @@ export default function UsersPage() {
   };
 
   // Generating user table rows
-  const tableRows = users.map(({lastName, firstName, id, username, email}, i) => ({
-    cells: [
-      {
-        // Name
-        key: lastName + firstName,
-        content: (
-          <Link href={`/users/${encodeURIComponent(id)}`}>
-            <a>
-              {lastName} {firstName}
-            </a>
-          </Link>
-        ),
-      },
-      {
-        // Username
-        key: username,
-        content: (
-          <Link href={`/users/${encodeURIComponent(id)}`}>
-            <a>
-              {username}
-            </a>
-          </Link>
-        ),
-      },
-      {
-        // Email
-        key: email,
-        content: email,
-      },
-    ],
-    key: username,
-  }));
-
-  const breadcrumbs = (
-    <BreadcrumbsStateless onExpand={() => {}}>
-      <BreadcrumbsItem text="Users" href="/UsersPage" />
-    </BreadcrumbsStateless>
+  const tableRows = users.map(
+    ({ lastName, firstName, id, username, email }, i) => ({
+      cells: [
+        {
+          // Name
+          key: lastName + firstName,
+          content: (
+            <Link href={`/users/${encodeURIComponent(id)}`}>
+              <a>
+                {lastName} {firstName}
+              </a>
+            </Link>
+          ),
+        },
+        {
+          // Username
+          key: username,
+          content: (
+            <Link href={`/users/${encodeURIComponent(id)}`}>
+              <a>{username}</a>
+            </Link>
+          ),
+        },
+        {
+          // Email
+          key: email,
+          content: email,
+        },
+      ],
+      key: username,
+    })
   );
 
-  // TODO: Come up with a prettier design, this is really ugly.
+  // TODO: use a banner instead
+  // render Error component
+  if (error) {
+    return <div>Error brah</div>;
+  }
+
   return (
     <Layout>
-      <PageHeader breadcrumbs={breadcrumbs}>
+      <PageHeader
+        breadcrumbs={
+          <BreadcrumbsStateless onExpand={() => {}}>
+            <BreadcrumbsItem text="Users" href="/UsersPage" />
+          </BreadcrumbsStateless>
+        }
+      >
         User list
       </PageHeader>
-      <section>
-        <DynamicTable
-          caption={null}
-          head={tableHeadRow}
-          rows={tableRows}
-          loadingSpinnerSize="large"
-          isLoading={false}
-          isFixedSize
-          defaultSortKey="Name"
-          defaultSortOrder="ASC"
-        />
-      </section>
+      {!error && !data ? (
+        <HugeSpinner />
+      ) : (
+        <section>
+          <DynamicTable
+            caption={null}
+            head={tableHeadRow}
+            rows={tableRows}
+            loadingSpinnerSize="large"
+            isLoading={false}
+            isFixedSize
+            defaultSortKey="Name"
+            defaultSortOrder="ASC"
+          />
+        </section>
+      )}
     </Layout>
   );
 }
