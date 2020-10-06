ca3c969b5b8ade6a1787b0d0665763efa8a5a2e0
Jakub Žitný <jakubzitny@avocode.com>
2020-10-06 13:36:29 +0200

    remove useless comments from Layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,31 +1,22 @@
-// documentation
-// Navigation: https://atlaskit.atlassian.com/packages/design-system/navigation-next
-// Page, Grid: https://atlaskit.atlassian.com/packages/design-system/page
-// Page Layout: https://atlaskit.atlassian.com/packages/design-system/page-layout
-
-// dependencies
 import React from "react";
 import { LayoutManager, NavigationProvider } from "@atlaskit/navigation-next";
 import styled from "styled-components";
 
-// grid and layout
 import Page from "@atlaskit/page";
 import { Content, PageLayout, Main } from "@atlaskit/page-layout";
 
-// navigation components
 import ContainerNavigation from "../components/ContainerNavigation/ContainerNavigation";
 import LSGlobalNavigation from "../components/LSGlobalNavigation/LSGlobalNavigation";
 
-interface ILayoutProps {
+interface IProps {
   children: React.ReactNode;
 }
 
-// styling <Main/>
 const Padding = styled.div`
   padding: 40px;
 `;
 
-const Layout = ({ children }: ILayoutProps) => (
+const Layout = ({ children }: IProps) => (
   <Page>
     <NavigationProvider>
       <LayoutManager
