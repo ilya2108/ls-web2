a3ac06b5af0f2175ef1f68d366bef25e93cef1fa
Jakub Žitný <zitnyjak@fit.cvut.cz>
2020-10-07 06:41:29 +0200

    add sentry

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
 import '@atlaskit/css-reset';
 
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://c6cc13fa595942959e487394f7e51680@o458142.ingest.sentry.io/5455173",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
