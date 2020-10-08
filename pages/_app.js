3b6ba3f39d2bcbf9ff90f5026a5405be6fdefe18
Jakub Žitný <zitnyjak@fit.cvut.cz>
2020-10-08 09:11:50 +0200

    initialize Sentry only on prod

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,16 @@ import '@atlaskit/css-reset';
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 
-Sentry.init({
-  dsn: "https://c6cc13fa595942959e487394f7e51680@o458142.ingest.sentry.io/5455173",
-  integrations: [
-    new Integrations.BrowserTracing(),
-  ],
+if (process.env.PROD) {
+  Sentry.init({
+    dsn: "https://c6cc13fa595942959e487394f7e51680@o458142.ingest.sentry.io/5455173",
+    integrations: [
+      new Integrations.BrowserTracing(),
+    ],
 
-  tracesSampleRate: 1.0,
-});
+    tracesSampleRate: 1.0,
+  });
+}
 
 
 function MyApp({ Component, pageProps }) {
