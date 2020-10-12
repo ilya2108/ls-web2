aacf4fdead7ced53650a60accea1ce392c33e7e0
Jakub Žitný <zitnyjak@fit.cvut.cz>
2020-10-12 21:11:37 +0200

    put maintenance mode to login page

--- components/Login.tsx
+++ components/Login.tsx
@@ -11,7 +11,8 @@ export default function Login() {
       <img className="logo-img" src="/logo.svg" />
       <div className="title">Learnshell</div>
       <br />
-      <a href={`https://auth.fit.cvut.cz/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`}>Please, login with your CTU account</a>
+      {/* <a href={`https://auth.fit.cvut.cz/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`}>Please, login with your CTU account</a> */}
+      <a href='#'>We're doing maintenance. We'll be back online soon. Sorry about that.</a>
     </div>
   )
 }
