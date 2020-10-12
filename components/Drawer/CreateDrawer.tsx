c3124ec00eb27dd44faf11bb0359b8c508a5f343
Jakub Žitný <zitnyjak@fit.cvut.cz>
2020-10-12 05:43:24 +0200

    remove boring comments from CreateDrawer

--- components/Drawer/CreateDrawer.tsx
+++ components/Drawer/CreateDrawer.tsx
@@ -1,16 +1,12 @@
-// documentation
-// https://atlaskit.atlassian.com/packages/design-system/drawer
-
-// dependencies
 import {
-    SideNavigation,
-    NavigationContent,
-    ButtonItem,
-  } from "@atlaskit/side-navigation";
+  SideNavigation,
+  NavigationContent,
+  ButtonItem,
+} from "@atlaskit/side-navigation";
 
-  import CalendarIcon from "@atlaskit/icon/glyph/calendar";
-  import EmojiObjectsIcon from "@atlaskit/icon/glyph/emoji/objects";
-  import EmojiNatureIcon from "@atlaskit/icon/glyph/emoji/nature";
+import CalendarIcon from "@atlaskit/icon/glyph/calendar";
+import EmojiObjectsIcon from "@atlaskit/icon/glyph/emoji/objects";
+import EmojiNatureIcon from "@atlaskit/icon/glyph/emoji/nature";
 
 
 const forbidden = () => {
@@ -21,15 +17,14 @@ const navigate = (where: string) => {
   window.location.pathname = `${where}`
 }
 
-// New SideNavigation
-const CreateDrawer: React.FunctionComponent = () => (
-    <SideNavigation label="project">
-    <NavigationContent>
-      <ButtonItem onClick={forbidden} iconBefore={<CalendarIcon label=""/>}>Start Exam <strong>right now</strong></ButtonItem>
-      <ButtonItem onClick={forbidden} iconBefore={<EmojiObjectsIcon label=""/>}>Create Exam</ButtonItem>
-      <ButtonItem onClick={() => navigate('/assignments/create')} iconBefore={<EmojiNatureIcon label=""/>}>Create Assignment</ButtonItem>
-    </NavigationContent>
-  </SideNavigation>
-);
-
-export default CreateDrawer;
+export default function CreateDrawer() {
+  return (
+      <SideNavigation label="project">
+      <NavigationContent>
+        <ButtonItem onClick={forbidden} iconBefore={<CalendarIcon label=""/>}>Start Exam <strong>right now</strong></ButtonItem>
+        <ButtonItem onClick={forbidden} iconBefore={<EmojiObjectsIcon label=""/>}>Create Exam</ButtonItem>
+        <ButtonItem onClick={() => navigate('/assignments/create')} iconBefore={<EmojiNatureIcon label=""/>}>Create Assignment</ButtonItem>
+      </NavigationContent>
+    </SideNavigation>
+  )
+}
