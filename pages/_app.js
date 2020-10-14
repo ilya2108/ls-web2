85339f0b7f3c4970bea4b2ae83114c19f1f088c0
Jakub Žitný <zitnyjak@fit.cvut.cz>
2020-10-14 02:00:11 +0200

    revert graphql-hooks usage for apollo for parallels page
    - it was bad idea to try new gql dep at 1am
    - keeping it in history though, the code with variables will be cleaner

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,19 @@
 import '@atlaskit/css-reset';
 import { Provider } from 'react-redux';
-import { GraphQLClient, ClientContext } from 'graphql-hooks'
-
 import { useStore } from '../modules/core/redux/store';
-import { initSentry } from '../modules/tracker'
 
 import '../styles/globals.css'
+import { initSentry } from '../modules/tracker'
 
 if (process.env.PROD) {
   initSentry()
 }
 
-const client = new GraphQLClient({
-  url: process.env.API_ENDPOINT ?? "/graphql/"
-})
-
 function MyApp({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState)
   return (
     <Provider store={store}>
-      <ClientContext.Provider value={client}>
-        <Component {...pageProps} />
-      </ClientContext.Provider>
+      <Component {...pageProps} />
     </Provider>
   )
 }
---
+++ pages/parallels/[parallelId].tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { gql } from "graphql-request";
+
+import { fetcher } from "../../modules/api";
+
+import Layout from "../../layout/Layout";
+import Loading from "../../components/Loading";
+import Error, { hasQueryError } from "../../components/Error";
+
+
+
+export default function Parallel() {
+  const router = useRouter();
+  const { parallelId } = router.query;
+  if (!parallelId) {
+    return <Loading />
+  }
+
+  const { data, error } = useSWR(
+    gql`
+      query getParallelDetails {
+        ParallelDetail(id: ${parallelId}) {
+          id
+          course {
+            id
+          }
+          students {
+            totalCount
+            results {
+              username
+              id
+            }
+          }
+        }
+      }
+    `,
+    fetcher
+  )
+
+  if (!data && !error) {
+    return <Loading />
+  }
+
+  if (error) {
+    return <Layout><Error errors={error} /></Layout>
+  }
+
+  const students = data?.ParallelDetail?.students?.results.map((student) => {
+    return student.username
+  })
+
+  console.log(students)
+
+  return (
+    <Layout>
+      <h1>Parallel: {parallelId}</h1>
+      <br />
+      <div className="parallels">
+        {students.join(" ")}
+      </div>
+    </Layout>
+  )
+}
