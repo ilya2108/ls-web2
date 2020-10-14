210fdd888f2c48f3116d49753cae752504cf6e23
Jakub Žitný <zitnyjak@fit.cvut.cz>
2020-10-14 03:19:07 +0200

    allow starting exams from parallels page

--- pages/parallels/[parallelId].tsx
+++ pages/parallels/[parallelId].tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import { gql } from "graphql-request";
@@ -17,6 +17,9 @@ export default function Parallel() {
   if (!parallelId) {
     return <Loading />
   }
+
+  const [chosenExamId, updateExamId] = useState(null)
+  const [status, setStatus] = useState('')
 
   const { data, error } = useSWR(
     gql`
@@ -39,31 +42,146 @@ export default function Parallel() {
     fetcher
   )
 
-  if (!data && !error) {
+  const { data: dataExams, error: errorExams } = useSWR(
+    gql`
+      query exams {
+        ExamList {
+          results {
+            id
+            template {
+              name
+            }
+          }
+        }
+      }
+    `,
+    fetcher
+  )
+
+  if ((!data && !error) || (!dataExams && !errorExams)) {
     return <Loading />
   }
 
-  if (error) {
-    return <Layout><Error errors={error} /></Layout>
+  if (error || errorExams) {
+    return <Layout><Error errors={{ error, errorExams }} /></Layout>
   }
 
-  const students = data?.ParallelDetail?.students?.results.map((student) => {
-    return student.username
+  const students = data?.ParallelDetail?.students?.results
+  const usernames = students.map(({ username }) => username)
+  const ids = students.map(({ id }) => id)
+  const availableExams = dataExams?.ExamList?.results.map(({ id, template }) => {
+    return {
+      id,
+      name: template.name
+    }
   })
 
-  console.log(students)
+  const handleExamChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    updateExamId(e.target.value)
+  }
+
+  const handleExamStudentAdd = () => {
+    // TODO: add students 1 by 1 to the exam
+
+    const jobs = ids.map((id) => {
+      return fetcher(gql`mutation addStudentToExam{
+        StudentWritesExamCreate(data: {examId: ${chosenExamId}, studentId: ${id}}){
+          object{
+            id
+            student{
+              username
+            }
+          }
+        }
+      }`)
+    })
+
+    Promise.all(jobs)
+      .then(() => {
+        setStatus('All students were added')
+      })
+      .catch((e) => {
+        setStatus('There were some errors')
+        console.error(e)
+      })
+    
+    setStatus('Students added, wait a while and complete the enrollment')
+  }
+  
+  const handleExamComplete = () => {
+    return fetcher(gql`mutation completeEnrollment{
+      ExamCompleteEnrollment(data: {id: ${chosenExamId}}){
+        job{
+          id
+        }
+      }
+    }`)
+    .then(() => {
+      setStatus('Exam enrollment is done, wait a while till Learnshell generates it and you can start!')
+    })
+    .catch(() => {
+      setStatus('We had problems with completing enrollment')
+    })
+  }
+  
+  const handleExamStart = () => {
+    return fetcher(gql`mutation startExam{
+      ExamStart(data: {id: 1}){
+        object{
+          id
+          startTime
+        }
+      }
+    }`)
+    .then(() => {
+      setStatus('Exam started!')
+    })
+    .catch(() => {
+      setStatus('We had prolems starting exam')
+    })
+  }
 
   return (
     <Layout>
       <h1>Parallel: {parallelId}</h1>
       <br />
+      <h3>Usernames:</h3>
       <div className="textarea-wrapper">
         <textarea
           className="textarea"
           rows={10}
           spellCheck="false"
           placeholder="student usernames"
-          defaultValue={students.join(" ")}
+          defaultValue={usernames.join(" ")}
+        />
+      </div>
+      <br />
+      <br />
+      <select onChange={handleExamChange}>
+        {availableExams.map((exam) => {
+          return <option value={exam.id}>{exam.name}</option>
+        })}
+      </select>
+      <button className="butt" onClick={() => handleExamStudentAdd()}>Add selected students to this Exam</button>
+      <button className="butt" onClick={() => handleExamComplete()}>Complete enrollment</button>
+      <button className="butt" onClick={() => handleExamStart()}>Start exam</button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="status">
+        {status}
+      </div>
+      <br />
+      <br />
+      <h3>IDs:</h3>
+      <div className="textarea-wrapper">
+        <textarea
+          className="textarea"
+          rows={10}
+          spellCheck="false"
+          placeholder="student ids"
+          defaultValue={ids.join(" ")}
         />
       </div>
     </Layout>
