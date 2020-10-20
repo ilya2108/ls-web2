85278cd7763797b1ef379c7dde29883bca646cc4
Jakub Žitný <zitnyjak@fit.cvut.cz>
2020-10-20 22:02:02 +0200

    back up

--- components/Login.tsx
+++ components/Login.tsx
@@ -17,8 +17,8 @@ export default function Login() {
       <img className="logo-img" src="/logo.svg" />
       <div className="title">Learnshell</div>
       <br />
-      {/* <b><a href={`https://auth.fit.cvut.cz/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`}>Please, login with your CTU account</a></b> */}
-      <a href='#'>We're doing maintenance. We'll be back online soon. Sorry about that.</a>
+      <b><a href={`https://auth.fit.cvut.cz/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`}>Please, login with your CTU account</a></b>
+      {/* <a href='#'>We're doing maintenance. We'll be back online soon. Sorry about that.</a> */}
       <br />
       <br />
       <br />
