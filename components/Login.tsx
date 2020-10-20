039c8352e82c9a708f1ca5e73e62af673990acb4
Jakub Žitný <zitnyjak@fit.cvut.cz>
2020-10-20 21:55:39 +0200

    maintenance bump

--- components/Login.tsx
+++ components/Login.tsx
@@ -17,12 +17,12 @@ export default function Login() {
       <img className="logo-img" src="/logo.svg" />
       <div className="title">Learnshell</div>
       <br />
-      <b><a href={`https://auth.fit.cvut.cz/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`}>Please, login with your CTU account</a></b>
+      {/* <b><a href={`https://auth.fit.cvut.cz/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`}>Please, login with your CTU account</a></b> */}
+      <a href='#'>We're doing maintenance. We'll be back online soon. Sorry about that.</a>
       <br />
       <br />
       <br />
       <span className="hints-toggle-handle" onClick={handleTipClick}>(doesn't work?)</span>
-      {/* <a href='#'>We're doing maintenance. We'll be back online soon. Sorry about that.</a> */}
       <br />
       {tip ? (
         <i className="login-hints">If it doesn't work, try logging out and back in over at <u><a href="https://auth.fit.cvut.cz/login">auth.fit.cvut.cz</a></u> separately, please.</i>
