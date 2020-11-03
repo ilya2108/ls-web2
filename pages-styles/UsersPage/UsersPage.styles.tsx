e1ee336283da087d6f83e857a10c9270a451ea5e
Tuan Le <tuanldt@outlook.com>
2020-11-03 23:44:58 +0100

    add search bar and search functionality

---
+++ pages-styles/UsersPage/UsersPage.styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+/* width: 500px; */
+`;
+export const SearchWrapper = styled.div`
+    width: 240px;
+    height: 32px;
+    margin-bottom: 20px;
+`;
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,7 +1,10 @@
+import React, { useState, Fragment } from "react";
 import Link from "next/link";
 import useSWR from "swr";
 import { gql } from "graphql-request";
 import { v4 } from 'uuid';
+import debounce from "lodash/debounce"
+
 
 import DynamicTable from "@atlaskit/dynamic-table";
 import PageHeader from "@atlaskit/page-header";
@@ -11,8 +14,25 @@ import Layout from "../../layout/Layout";
 import { fetcher } from "../../modules/api";
 import HugeSpinner from "../../components/HugeSpinner/HugeSpinner";
 import Error from "../../components/Error";
+import { calculateSemesterScore } from '../../utils/score-utils'
+import Textfield from "@atlaskit/textfield";
+import { SearchWrapper } from "../../pages-styles/UsersPage/UsersPage.styles";
+import EditorSearchIcon from "@atlaskit/icon/glyph/editor/search";
+import ShortcutIcon from "@atlaskit/icon/glyph/shortcut";
+import Button from '@atlaskit/button';
+
 
 export default function UsersPage() {
+  const [inputVal, setInputVal] = useState("");
+  // TODO: figure out a way to render a loading state
+  const [loading, setLoading] = useState(false);
+  const setInputValDebounced = debounce(setInputVal, 250)
+  const handleSearchEvent = (event) => {
+    const { value } = event.target;
+    setInputValDebounced(value)
+  };
+
+
   const { data, error } = useSWR(
     gql`
       {
@@ -48,8 +68,18 @@ export default function UsersPage() {
   };
 
   // Generating user table rows
-  const tableRows = users.map(
-    ({ lastName, firstName, id, username, email }, i) => {
+
+  const filterUsers = (users) => {
+    const filteredUsers = users.filter((user) => {
+      return user.username.toLowerCase().includes(inputVal.toLowerCase());
+    });
+    return filteredUsers;
+  };
+
+
+  const tableRows = filterUsers(users).map(
+    ({ lastName, firstName, id, username, email, assignments }, i) => {
+      const score = calculateSemesterScore(assignments)
       return {
         cells: [
           {
@@ -101,13 +131,21 @@ export default function UsersPage() {
         }
       >
         User list
-      </PageHeader>
+        </PageHeader>
       {!error && !data ? (
         <HugeSpinner />
       ) : (
-        <section>
+        <Fragment>
+          <SearchWrapper>
+            <Textfield
+              name="basic"
+              isCompact
+              placeholder="Search username"
+              elemAfterInput={<EditorSearchIcon label="" />}
+              onChange={(event) => handleSearchEvent(event)}
+            />
+          </SearchWrapper>
           <DynamicTable
-            caption={null}
             head={tableHeadRow}
             rows={tableRows}
             loadingSpinnerSize="large"
@@ -116,8 +154,9 @@ export default function UsersPage() {
             defaultSortKey="Name"
             defaultSortOrder="ASC"
           />
-        </section>
+        </Fragment>
       )}
+
     </Layout>
   );
 }
