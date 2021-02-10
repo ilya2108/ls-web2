4fb3dab490b6824e2bf781756f444bf75b01c9d7
juriczby <juriczby.fit.cvut.cz>
2021-02-10 21:17:40 +0100

    add component for listing plagiats

---
+++ components/Plagiarism/PlagiatsList.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Link from "next/link";
+
+import Badge from '@atlaskit/badge';
+import Lozenge from '@atlaskit/lozenge';
+
+export default function PlagiatsList({ plagiats }) {
+    return (
+        <div>
+            {plagiats.map((plagiat, index) => {
+                return (
+                    <div key={index} className="plagiat-wrapper">
+                        <div className="plagiat-header">
+                            <Link href={`/plagiarism/assignment/${encodeURIComponent(plagiat.culprit_assignment_name)}`}>
+                                <h1><a>{plagiat.culprit_assignment_name}</a></h1>
+                            </Link>
+                            <Badge>{"Count: " + plagiat.culprit_count}</Badge>
+                        </div>
+                        <code>{plagiat.script}</code>
+                        <h2>Culprits:</h2><br />
+                        <div className="culprits">
+                            {plagiat.culprits.map((culprit, index) => {
+                                return (
+                                    <span key={index}>
+                                        <Lozenge>
+                                            <Link href={`/plagiarism/user/${encodeURIComponent(culprit)}`}>
+                                                <a>{culprit}</a>
+                                            </Link>
+                                        </Lozenge>
+                                    </span>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    );
+};
+
+PlagiatsList;
--- pages/plagiarism/index.tsx
+++ pages/plagiarism/index.tsx
@@ -1,38 +1,14 @@
 import React from 'react';
 import Layout from "../../layout/Layout";
 
-import Badge from '@atlaskit/badge';
-import Lozenge from '@atlaskit/lozenge';
-
 import plagiats from './json/res_semester_anonymized_pretty.json';
+
+import PlagiatsList from '../../components/Plagiarism/PlagiatsList';
 
 export default function users() {
     return (
         <Layout>
-            {/* TODO - Move to its own component */}
-            {plagiats.map((plagiat, index) => {
-                return (
-                    <div key={index} className="plagiat-wrapper">
-                        <div className="plagiat-header">
-                            {/* TODO - Change to link */}
-                            <h1>{plagiat.culprit_assignment_name}</h1>
-                            <Badge>{"Count: " + plagiat.culprit_count}</Badge>
-                        </div>
-                        <code>{plagiat.script}</code>
-                        <h2>Culprits:</h2><br />
-                        <div className="culprits">
-                            {plagiat.culprits.map((culprit, index) => {
-                                return (
-                                    <span key={index}>
-                                        {/* TODO - Change to link */}
-                                        <Lozenge>{culprit}</Lozenge>
-                                    </span>
-                                )
-                            })}
-                        </div>
-                    </div>
-                )
-            })}
+            <PlagiatsList plagiats={plagiats} />
         </Layout>
     );
 }
